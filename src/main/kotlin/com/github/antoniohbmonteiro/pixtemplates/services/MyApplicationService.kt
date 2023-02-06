b059c1e58cb5bd986439323a2fa46682e7cd9a11
GitHub Action <action@github.com>
2023-02-06 19:50:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/antoniohbmonteiro/pixtemplates/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/antoniohbmonteiro/pixtemplates/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.antoniohbmonteiro.pixtemplates.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.antoniohbmonteiro.pixtemplates.MyBundle
 
 class MyApplicationService {
 
